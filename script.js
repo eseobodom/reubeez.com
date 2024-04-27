75db768c41438c09555331c0e2b9ed2eb704e500
Eseabasi Obodom <146686307+secretdetective@users.noreply.github.com>
2024-04-27 16:12:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let interval = setInterval(writeNextLetter, 100);
 
 }
 
-writeText('HEY! WELCOME TO REUBEEZ... Godspower');
+writeText('HEY! WELCOME TO REUBEEZ...');
 
 
 function isElementInViewport(el) {
